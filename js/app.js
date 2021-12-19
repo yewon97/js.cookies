10dac8cbd1db5285aa7d4dced45026623aff72a9
yewon97 <dpdnjs9936@gmail.com>
2021-12-20 02:30:43 +0900

    셀에 홍길동이라는 textNode를 삽입

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 window.onload = () => {
   // 버튼 가져오기
-  const btnCrc = document.querySelector(".btnCrc");
+  const btnCrc = document.querySelector('.btnCrc');
 
   // 버튼 클릭 시 --> 할일
   btnCrc.addEventListener('click', () => {
@@ -8,7 +8,7 @@ window.onload = () => {
     const hTbody = document.getElementById('htmlTbody');
     console.log(hTbody);
 
-    /* js table row and column */
+/* js table row and column */
     // insert a row at the end of table
     const newRow0 = hTbody.insertRow(); // 통짜로 된 row 한줄 추가 tr이 추가됨
     console.log(newRow0);
@@ -18,6 +18,10 @@ window.onload = () => {
     const newCell1 = newRow0.insertCell();
     const newCell2 = newRow0.insertCell();
     const newCell3 = newRow0.insertCell();
+/* 버튼을 클릭할 때마다 tr>td*4 생성됨 */
 
-  })
-}
+    // Append - 텍스트 노드를 새롭게 생성한 Cell에 붙이기
+    const newText0 = document.createTextNode('홍길동');
+    newCell0.appendChild(newText0);
+  });
+};
