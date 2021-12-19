9cbc5e02b1577c82236713092f5b5834f40b5dea
yewon97 <dpdnjs9936@gmail.com>
2021-12-20 02:25:52 +0900

    생성된 tr한줄에 td만들기

--- js/app.js
+++ js/app.js
@@ -10,9 +10,14 @@ window.onload = () => {
 
     /* js table row and column */
     // insert a row at the end of table
-    const newRow = hTbody.insertRow(); // 통짜로 된 row 한줄 추가 tr이 추가됨
-    console.log(newRow);
+    const newRow0 = hTbody.insertRow(); // 통짜로 된 row 한줄 추가 tr이 추가됨
+    console.log(newRow0);
 
-    // 
+    // insert a cell at the end of the row
+    const newCell0 = newRow0.insertCell();
+    const newCell1 = newRow0.insertCell();
+    const newCell2 = newRow0.insertCell();
+    const newCell3 = newRow0.insertCell();
+
   })
 }
